e3ee4bc91ec48a4d9745f9ba811dd729322e8a95
Khaja Dudekela <khaja.dudekela@pixacore.com>
2023-10-06 10:52:58 +0530

    parallel

--- cypress.config.js
+++ cypress.config.js
@@ -5,13 +5,25 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://www.google.com/", // Move baseUrl under the e2e testing type property
     specPattern: "**/*.cy.js", // Update testFiles to specPattern
   },
-  projectId: "z2d7pq", // Replace with your actual project ID from Cypress Dashboard
-  parallel: true,
-  video: true, // Disable video recording if not needed
+  videosFolder: 'cypress/videos', // Define the folder where videos will be saved
+  screenshotsFolder: 'cypress/screenshots', // Define the folder where screenshots will be saved
+  integrationFolder: 'cypress/integration', // Define the folder where your test files are located
+  baseUrl: "https://www.google.com/", // Set your baseUrl here
+
+  // Cypress Dashboard configuration
+  projectId: "z2d7pq", // Your actual project ID from Cypress Dashboard
+  record: true, // Set to true to enable recording on Cypress Dashboard
+
+  // Parallelization configuration
+  numTestsKeptInMemory: 5, // Number of tests kept in memory
+  numTestsKeptInDisk: 20, // Number of tests kept on disk
+  parallel: true, // Enable parallel test runs
+
+  // Other Cypress options can be added here as needed
 });
+
 
 // npx cypress run --record --key 67d2d553-e8df-4548-aa05-bc09aaa94c35
 // npm run cypress:run -- --record --key 67d2d553-e8df-4548-aa05-bc09aaa94c35 --parallel
