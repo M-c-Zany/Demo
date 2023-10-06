c52a012831d5b4ab1e86408300b63829d388e7cc
Khaja Dudekela <khaja.dudekela@pixacore.com>
2023-10-06 11:14:24 +0530

    01

--- cypress.config.js
+++ cypress.config.js
@@ -5,24 +5,23 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "**/*.cy.js", // Update testFiles to specPattern
+    specPattern: "cypress/e2e/**/*.cy.js", // Update specPattern here
+    baseUrl: "https://www.google.com/", // Set your baseUrl here
   },
-  videosFolder: 'cypress/videos', // Define the folder where videos will be saved
-  screenshotsFolder: 'cypress/screenshots', // Define the folder where screenshots will be saved
-  integrationFolder: 'cypress/integration', // Define the folder where your test files are located
-  baseUrl: "https://www.google.com/", // Set your baseUrl here
-
+  videosFolder: 'cypress/videos',
+  screenshotsFolder: 'cypress/screenshots',
+  
   // Cypress Dashboard configuration
-  projectId: "z2d7pq", // Your actual project ID from Cypress Dashboard
-  record: true, // Set to true to enable recording on Cypress Dashboard
-
+  projectId: "z2d7pq",
+  record: true,
+  
   // Parallelization configuration
-  numTestsKeptInMemory: 5, // Number of tests kept in memory
-  numTestsKeptInDisk: 20, // Number of tests kept on disk
-  parallel: true, // Enable parallel test runs
-
-  // Other Cypress options can be added here as needed
+  numTestsKeptInMemory: 5,
+  numTestsKeptInDisk: 20,
+  parallel: true,
 });
+
+
 
 
 // npx cypress run --record --key 67d2d553-e8df-4548-aa05-bc09aaa94c35
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,19 @@
 // auth.spec.js
+const specificPageUrl = Cypress.config("baseUrl") + 'path/to/specific/page';
+    
 describe('User Authentication', () => {
     it('should allow user login', () => {
-      cy.visit("www.google.com")
+      cy.visit(specificPageUrl);
     });
   
     it('should allow user registration', () => {
-        cy.visit("www.google.com")
+      cy.visit(specificPageUrl);
     });
     it('should allow user registration', () => {
-      cy.visit("www.google.com")
+      cy.visit(specificPageUrl);
   });
   it('should allow user registration', () => {
-    cy.visit("www.google.com")
+    cy.visit(specificPageUrl);
 });
   });
   
